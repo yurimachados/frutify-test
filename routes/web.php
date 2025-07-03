aa6383da1a747332d82997b2fda031772201cd7c
Yuri Machado <ymachado1995@gmail.com>
2025-07-03 11:08:09 -0300

    feat: reorganiza rotas de contatos e adiciona controlador de exclusão

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
-use App\Http\Controllers\CreateContactController;
+use App\Http\Controllers\Contacts\CreateContactController;
+use App\Http\Controllers\Contacts\ContactController;
+use App\Http\Controllers\Contacts\DeleteContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/contacts', CreateContactController::class);
+// Contact routes
+Route::group(['prefix' => 'contacts'], function () {
+    Route::post('/', CreateContactController::class)->name('contacts.store');
+    Route::delete('/{contactId}', DeleteContactController::class)->name('contacts.destroy');
+
+
+    Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get('/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+    Route::get('/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+});
+
